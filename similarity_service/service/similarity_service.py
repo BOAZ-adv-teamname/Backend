ab4b8fa3b6063948c58903da7d3c5617f4eda875
Kim Minyoung <61506233+kkminyoung@users.noreply.github.com>
2021-07-18 11:46:55 +0900

    update similarity (sentencebert)

--- similarity_service/service/similarity_service.py
+++ similarity_service/service/similarity_service.py
@@ -1,29 +1,34 @@
+import torch
+from sentence_transformers import SentenceTransformer, util
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 from model.precedent_dao import *
 
 def vectorization(DATABASE,NEWS):
-    database =  DATABASE
-    news = NEWS
+    model_path = 'paraphrase-multilingual-mpnet-base-v2'
+    embedder = SentenceTransformer(model_path)
+    query = NEWS
 
-    corpus=[x for x in database]
-    corpus.append(news)
-    vectorizer = TfidfVectorizer()
-    X = vectorizer.fit_transform(corpus)
+    corpus_embeddings = torch.load('./model/Sum_Database/law_encoder.pt',map_location=torch.device('cpu'))
+    query_embedding = embedder.encode(query, convert_to_tensor=True)
+    X = (corpus_embeddings,query_embedding)
     return X
 
 def Cosine_similarity(X,DATABASE):
-    news = X[-1]
-    laws = X[:-1]
-    database=DATABASE
-    sim = cosine_similarity(news,laws)[0]
-    result = pd.DataFrame({'corpus':database,
-                            'similarity':sim})
-    final = result.sort_values('similarity').iloc[:3,0]
+    top_k = 3
+    news = X[1]
+    laws = X[0]
+    cos_scores = util.pytorch_cos_sim(news, laws)[0]
+    cos_scores = cos_scores.cpu()
 
+    top_results = np.argpartition(-cos_scores, range(top_k))[0:top_k]
+
+    final=[]
+    for idx in top_results[0:top_k]:
+        final.append(DATABASE[idx].strip())
     return final
-
+    
 def make_simtext(news):
     DATABASE = get_precedent_data(local=True)
     NEWS = news
@@ -34,7 +39,7 @@ def make_simtext(news):
     for i,f in enumerate(final):
         output.append(f)
 
-        text_file = open(f"./service/Output/output_{i+1}.txt", "w")
+        text_file = open(f"./service/Output/output_{i+1}.txt", "w",encoding='UTF-8')
         n = text_file.write(f)
         text_file.close()
     
